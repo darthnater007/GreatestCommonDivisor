6a80ce21e54f0a31730a30f4d25495b7319ab317
darthnater007 <nsperk@gmail.com>
2018-03-14 21:49:24 -0400

    Calculates Greatest Common Divisor between two integer-wont' work with non-divisible numbers

---
+++ src/GreatestDivisor.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class GreatestDivisor {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Wecome to the Greatest Divisor App");
+		
+		Scanner sc = new Scanner(System.in);
+		String choice= "Y";
+		while(choice.equalsIgnoreCase("Y")) 
+		{
+		
+		System.out.print("Enter an integer:  ");
+		int y = sc.nextInt();
+		System.out.print("Enter another integer:  ");
+		int x = sc.nextInt();
+			while (x != y)
+			{
+				if (y > x) y -= x;
+				else x-=y;
+			}
+			
+			System.out.println(x + " is GCD.");
+			System.out.print("Continue?  y/n:  ");
+			choice = sc.next();
+		}
+
+	}
+
+}
